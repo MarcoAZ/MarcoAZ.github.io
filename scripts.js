7305b7707c07ef420da2eb5146c03b1b69e465df
Marco Zamora <marco.zamora@comcast.net>
2015-03-08 22:03:16 -0500

    first commit: redesign

--- scripts.js
+++ scripts.js
@@ -1,4 +1,36 @@
 $( document ).ready(function() {
-	// Your code here.
-	
+	//start hidden
+	$('.headerShrink').hide();
+	$('.navShrink').hide();
+
+	//control header shrink
+	$(window).scroll(function() {
+    if ($(document).scrollTop() > 100) 
+    {
+        $('.header').hide();
+        $('.headerShrink').show();
+        $('.ribbon').hide();
+        $('.nav').addClass('navExpand');
+    }
+    else 
+    {
+        $('.header').show();
+        $('.headerShrink').hide();
+        $('.ribbon').show();
+        $('.nav').removeClass('navExpand');
+    }
+	//control nav shrink
+    if ($(document).scrollTop() > 200) 
+    {
+		$('.navShrink').show();
+		$('.nav').hide();
+		$('.navExpand').hide();
+    }
+    else
+    {
+   		 $('.navShrink').hide();
+   		 $('.nav').show();
+    }
+    
+	});
 });
